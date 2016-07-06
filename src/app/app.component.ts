94d1f62816d1eebdb610afd2e8ddac23e4219cb4
Jek Bao CHOO <jerchoo@outlook.com>
2016-07-06 14:02:48 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import { DialogService } from './dialog.service';
+import { HeroService } from './heroes/hero.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  providers: [HeroService, DialogService],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/dialog.service.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by jerchoo on 6/7/16.
+ */
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class DialogService {
+    confirm(message?:string) {
+        return new Promise<boolean>(resolve => {
+            return resolve(window.confirm(message || 'Is it OK?'));
+        });
+    }
+}
